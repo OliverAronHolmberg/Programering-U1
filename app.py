9969678ae789195f668307ea9d48dc4ce4ee3cf5
OliverAronHolmberg <oliver@systementor.se>
2025-08-27 13:39:04 +0200

    Uppgift klar

---
+++ app.py
@@ -0,0 +1,43 @@
+
+#UPPGIFTER
+
+# 1. Skriv ut "Hello World" på skärmen
+print("Hello world!")
+
+# 2. Printa ut en text och ha med ett citat
+print("Hej \"Olvier\"")
+
+# 3. Skriv “Lorem Ipsum” tio gånger under varandra med trippel citattecken
+print("""
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+      """)
+
+# 4. Skriv flera saker i en och samma print() med \n
+print("Hej\nDetta\nÄr\nFlera\nRader")
+
+
+# 5. Sammanfoga (konkatenera) strängar med hjälp av plus-tecken: print(“Fy “ + “17!”)
+print("Hej "+ "jag heter Oliver")
+
+
+# 7. Ta in data genom input och skriva ut på skärmen
+# 8. Välkomstprogram med input
+# - ha med ditt namn på något sätt
+
+name = input("Skriv ditt namn: ")
+age = input("Skriv din ålder: ")
+
+print(f"Välkommen {name} du är {age}")
+
+
+
+
